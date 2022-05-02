d4a7ae6ac142e4479da7ef6b1f1fc8f5d6dea4a4
Doyoung <coby5502@gmail.com>
2022-05-02 11:11:36 +0900

    뷰모델 고민중

--- MBTI/ViewModel/UserViewModel.swift
+++ MBTI/ViewModel/UserViewModel.swift
@@ -7,14 +7,5 @@
 
 import SwiftUI
 
-struct UserViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct UserViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        UserViewModel()
-    }
+class UserViewModel: ObservableObject {
 }
